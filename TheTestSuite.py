192e48ff0e83afc7863c8717bc160fe36e9abb24
Elizabeth <elizabethsmith219@gmail.com>
2018-01-27 14:00:32 -0600

    made some test cases

--- TheTestSuite.py
+++ TheTestSuite.py
@@ -1,19 +1,64 @@
 import unittest
-import test
+import database
+import sqlite3
+import datetime
 
 class Testing(unittest.TestCase):
-    
-    def test_1(self):
-        self.assertTrue(test.sayHi() == 'hello')
         
-    def test_2(self):
-        self.assertFalse(test.sayHi() == 'hello')
-
+    def test_create_connection(self):
+        self.assertIsNotNone(database.create_connection("ZooManagement.db"))
+        
+    def test_select(self):
+        self.assertIsNone(database.select(database.create_connection("ZooManagement.db"), "Supplies_In_Stock"))
+        
+    def test_makeGUI(self):
+        self.assertIsNone(database.makeGUI())
+        
+    def test_edit_cell(self):
+        self.assertIsNone(database.edit_cell(database.create_connection("ZooManagement.db"), "dinosaurs", "age", 7, "T-Rex"))
+        
+    def test_update_days_since_bath(self):
+        self.assertIsNone(database.update_days_since_bath(database.create_connection("ZooManagement.db")))
+        
+    def test_wash_dino(self):
+        conn = database.create_connection("ZooManagement.db")
+        curr = conn.cursor()
+        database.wash_dino(curr, "Hippo")
+        curr.execute("select Day_Of_Last_Bath from dinosaurs where type = 'Hippo'")
+        self.assertEqual(curr.fetchone()[0], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        
+    def test_wash_all_dinos(self):
+        conn = database.create_connection("ZooManagement.db")
+        curr = conn.cursor()
+        database.wash_all_dinos(curr)
+        curr.execute("select Day_Of_Last_Bath from dinosaurs")
+        for i in curr.fetchall():
+            self.assertEqual(i[0], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            
+    def test_populate_food_types(self):
+       conn = database.create_connection("ZooManagement.db")
+       curr = conn.cursor()
+       database.populate_food_types(curr)
+       curr.execute("select Type_Of_Food from dinosaurs")
+       self.assertIsNotNone(curr.fetchall())
+       
+    def test_populate_food_per_day(self):
+       conn = database.create_connection("ZooManagement.db")
+       curr = conn.cursor()
+       database.populate_food_per_day(curr)
+       curr.execute("select Pounds_Of_Food_Per_Day from dinosaurs")
+       self.assertIsNotNone(curr.fetchall())
+       
+    def test_populate_supplies_in_stock(self):
+       conn = database.create_connection("ZooManagement.db")
+       curr = conn.cursor()
+       database.populate_supplies_in_stock(curr)
+       curr.execute("select amount from Supplies_In_Stock")
+       self.assertIsNotNone(curr.fetchall())
+       
+   # def test_feed_dinos():
+    #    
+    
         
 if __name__ == '__main__':
     unittest.main()
-    
-#example of calling the test case. this would be in another program
-#def sayHi():
- #   word = 'hello'
- #   return word
